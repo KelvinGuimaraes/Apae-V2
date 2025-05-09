4278864449e46ca415d8673b38ab914b8f49eb46
DEV1GOR <igormatheus.0702@gmail.com>
2025-05-09 13:08:48 -0300

    adicionar imagens e áudios dos animais com funcionalidade de reprodução controlada

--- src/js/animais.js
+++ src/js/animais.js
@@ -1,1 +1,25 @@
-console
+let audioAtual = null; 
+
+function mostrarGrupo(grupo) {
+  const grupos = document.querySelectorAll('.grupo');
+  
+  grupos.forEach(g => g.style.display = 'none'); 
+  document.getElementById(grupo).style.display = 'flex'; 
+}
+
+function tocarSom(caminhoSom) {
+  if (!caminhoSom || caminhoSom === '#') {
+    alert("Som não disponível ainda.");
+    return;
+  }
+
+  
+  if (audioAtual) {
+    audioAtual.pause();
+    audioAtual.currentTime = 0;
+  }
+
+  
+  audioAtual = new Audio(caminhoSom);
+  audioAtual.play();
+}
